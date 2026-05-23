4c0af368d10c501deae817add17cba8df2046ac2
Rathanak324 <sereywathrathanak@gmail.com>
2026-05-23 11:39:32 +0700

    Second commit

--- EX-3/server.js
+++ EX-3/server.js
@@ -5,8 +5,11 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json())
+
+app.use(logRequest);
+
 // Route: GET /departments/:dept/courses
-app.get('/departments/:dept/courses', (req, res) => {
+app.get('/departments/:dept/courses', validateQueryParams, (req, res) => {
     const { dept } = req.params;
     const { level, minCredits, maxCredits, semester, instructor } = req.query;
     // Implementing the filter logic
@@ -26,7 +29,35 @@ app.get('/departments/:dept/courses', (req, res) => {
     res.json(FilterCourcses);
 });
 
+function logRequest(req, res, next) {
+    console.log({
+        method: req.method,
+        requestedpath: req.path,
+        query: { ...req.query },
+        timestamp: new Date().toISOString()
+    });
+    next();
+};
 
+function validateQueryParams(req, res, next) {
+    const { level, minCredits, maxCredits, semester, instructor } = req.query;
+
+    if (minCredits !== undefined && !Number.isInteger(Number(minCredits))) {
+        return res.status(400).json({ error: 'Invalid minCredits parameter' });
+    }
+
+    if (maxCredits !== undefined && !Number.isInteger(Number(maxCredits))) {
+        return res.status(400).json({ error: 'Invalid maxCredits parameter' });
+    }
+
+    if (minCredits !== undefined && maxCredits !== undefined) {
+        if (Number(minCredits) > Number(maxCredits)) {
+            return res.status(400).json({ error: 'minCredits cannot be greater than maxCredits' });
+        }
+    }
+
+    next();
+};
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
